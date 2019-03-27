b167d7219dd9c4fdecd71aec40cafeb6cf0a7588
aldamr01 <aldionamirrul@gmail.com>
2019-03-27 08:46:56 +0700

    add multiprocessing python

---
+++ Tugas 2/program/bilanganAcak.py
@@ -0,0 +1,7 @@
+import random
+
+def bilanganAcak(n):
+    for i in range(0,10):
+        print(random.randrange(0,n))
+
+bilanganAcak(2000)
---
+++ Tugas 2/program/withMultiProcessing.py
@@ -0,0 +1,34 @@
+import random
+import time
+import multiprocessing
+
+def SoalA():
+    result = []
+    for i in range(0,10):
+        if i%2==0:
+            result.append(i)
+    print(i)  
+
+def SoalB():
+    result = []
+    for i in range(50,60):
+        if i%2==1:
+            result.append(i)
+    print(result)
+
+def SoalC():
+    result = []
+    for i in range(0,5):
+        result.append(random.randrange(100,500))
+    print(result)
+
+if __name__ == "__main__":
+    worker1 = multiprocessing.Process(target=SoalA)
+    worker2 = multiprocessing.Process(target=SoalB)
+    worker3 = multiprocessing.Process(target=SoalC)
+    worker1.start()
+    worker2.start()
+    worker3.start()
+    worker1.join()
+    worker2.join()
+    worker3.join()
