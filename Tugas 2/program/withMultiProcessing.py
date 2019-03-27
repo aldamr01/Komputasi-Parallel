6eef5f9e58209d4ff62fd115f236e6fe71ff4acc
aldamr01 <aldionamirrul@gmail.com>
2019-03-27 11:00:51 +0700

    remove bug coy , wkwk

--- Tugas 2/program/withMultiProcessing.py
+++ Tugas 2/program/withMultiProcessing.py
@@ -5,14 +5,14 @@ import multiprocessing
 def SoalA():
     result = []
     for i in range(0,10):
-        if i%2==0:
+        if i%2==1:
             result.append(i)
     print(result)  
 
 def SoalB():
     result = []
     for i in range(50,60):
-        if i%2==1:
+        if i%2==0:
             result.append(i)
     print(result)
 
