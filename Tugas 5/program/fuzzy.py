e06e759bfbeedf3cabb4204a54a6d600009807b6
aldamr01 <aldionamirrul@gmail.com>
2019-04-24 19:16:00 +0700

    add tugas 5 , sudah dinilai

--- Tugas 5/program/fuzzy.py
+++ Tugas 5/program/fuzzy.py
@@ -1,27 +1,66 @@
-def command(p1,p2,p3,p4):
-    if p3 == 0 :
-        return print("NABRAK !!!!")
-    elif p3 < 400 :
-        return print("Kurangi kecepatan !!!") 
-    elif p3 >400 and p3 <500 :
-        return print("Belok ke kanan")
+def command(p1,p2,p3,p4,trafic):
+
+
+    # if p3 == 0 :
+    #     return print("NABRAK !!!!")
+    # elif p3 < 400 :
+    #     return print("Kurangi kecepatan !!!") 
+    # elif p3 >400 and p3 <500 :
+    #     return print("Belok ke kanan")
     
 
-    if p2 <= 0 :
-        return print("NABRAK !!!!")
-    if p3 <= 0 :
-        return print("NABRAK !!!!")
-    if p4 <= 0 :
-        return print("NABRAK !!!!")
+    # if p2 <= 0 :
+    #     return print("NABRAK !!!!")
+    # if p3 <= 0 :
+    #     return print("NABRAK !!!!")
+    # if p4 <= 0 :
+    #     return print("NABRAK !!!!")
         
 
     if p1 < p2 and p1 < p3 and p1 < p4 :
-        return print('Rem mobil sekarang !') 
-    elif p2 < p1 and p2 < p3 and p2 < p4 :
-        return print('Rem mobil sekarang !')
-    elif p3 < p1 and p3 < p2 and p3 < p4 :
-        return print('Belok kanan sekarang !')
-    elif p4 < p1 and p4 < p2 and p4 < p3 :
-        return print('Belok kanan sekarang !')
+        if trafic == 'merah' :
+            print('Berhenti sekarang , lampu traffic lagi merah') 
+        elif trafic == 'kuning' :
+            print('Melaju pelan-pelan , lampu traffic lagi kuning') 
+        else:
+            if p1 <= 10 :
+                print('Rem sekarang , didepan ada halangan') 
+            elif p1 >= 10 and p1 < 20:
+                print('Berjalan perlahan sekarang , didepan akan ada halangan') 
+            elif p1 >= 50 :
+                print('Melaju kedepan sekarang') 
 
+    elif p2 < p1 and p2 < p3 and p2 < p4 :       
+        if p2 <= 10 :
+            print('Rem sekarang , dibelakang ada halangan') 
+        elif p2 >= 10 and p2 < 20:
+            print('mundur perlahan sekarang , awas dibelakang akan ada halangan') 
+        elif p2 >= 50 :
+            print('Mundur kebelakang')
+
+    elif p3 < p1 and p3 < p2 and p3 < p4 :
+        if trafic == 'merah' :
+            print('Berhenti sekarang , lampu traffic lagi merah') 
+        elif trafic == 'kuning' :
+            print('Melaju pelan-pelan , lampu traffic lagi kuning') 
+        else:
+            if p3 <= 10 :
+                print('Belok ke kanan sekarang , disamping akan menabrak') 
+            elif p3 >= 10 and p3 < 20:
+                print('Belok nya pelan pelan , disamping akan ada halangan') 
+            elif p3 >= 50 :
+                print('Silahkan belok ke kiri bebas , tidak ada halangan terdeteksi') 
+
+    elif p4 < p3 and p4 < p2 and p4 < p1 :
+        if trafic == 'merah' :
+            print('Berhenti sekarang , lampu traffic lagi merah') 
+        elif trafic == 'kuning' :
+            print('Melaju pelan-pelan , lampu traffic lagi kuning') 
+        else:
+            if p3 <= 10 :
+                print('Belok ke kiri sekarang , disamping akan menabrak') 
+            elif p3 >= 10 and p3 < 20:
+                print('Belok nya pelan pelan , disamping akan ada halangan') 
+            elif p3 >= 50 :
+                print('Silahkan belok ke kanan bebas , tidak ada halangan terdeteksi') 
     
--- Tugas 5/program/soal1.py
+++ Tugas 5/program/soal1.py
@@ -5,38 +5,46 @@ import time
 
 
 def SensorDepan(depan):
-    Jdepan = random.randrange(1,2000)
+    Jdepan = random.randrange(1,100)
     depan.send(Jdepan)
     print('Jarak depan mobil : ',Jdepan,' cm')
     depan.close()
 
 def SensorBelakang(belakang):
-    Jbelakang = random.randrange(1,1000)
+    Jbelakang = random.randrange(1,100)
     belakang.send(Jbelakang)
     print('Jarak belakang mobil : ',Jbelakang,' cm')
     belakang.close()
 
 def SensorKiri(kiri):
     global Jkiri 
-    Jkiri = random.randrange(1,2000)
+    Jkiri = random.randrange(1,100)
     kiri.send(Jkiri)
     print('Jarak kiri mobil : ',Jkiri,' cm')
     kiri.close()
 
 def SensorKanan(kanan):
-    Jkanan = random.randrange(1,2000)
+    Jkanan = random.randrange(1,100)
     kanan.send(Jkanan)
     print('Jarak kanan mobil : ',Jkanan,' cm')
     kanan.close()
 
-def Controller(depan,belakang,kiri,kanan):
+def SensorTraffic(traffic):
+    list = ['merah','kuning','hijau']
+    lampu = random.choice(list)
+    traffic.send(lampu)
+    print('Lampu traffic sedang : ',lampu)
+    traffic.close()
+
+def Controller(depan,belakang,kiri,kanan,lampu):
     DP = depan.recv()
     BK = belakang.recv()
     KI = kiri.recv()
     KA = kanan.recv()
-    
+    LP = lampu.recv()
+
     print('--- Tindakan ---')
-    command(DP,BK,KI,KA)
+    command(DP,BK,KI,KA,LP)
 
 if __name__ == '__main__':
     while(1):
@@ -44,19 +52,23 @@ if __name__ == '__main__':
         belakangIn,belakangOut  = Pipe()
         kiriIn,kiriOut          = Pipe()
         kananIn,kananOut        = Pipe()
+        lampuIn,lampuOut        = Pipe()
         
         KirimSensorDepan        = Process(target=SensorDepan,args=(depanIn,))
         KirimSensorBelakang     = Process(target=SensorBelakang,args=(belakangIn,))
         KirimSensorKiri         = Process(target=SensorKiri,args=(kiriIn,))
         KirimSensorKanan        = Process(target=SensorKanan,args=(kananIn,))
-        ControllerUtama         = Process(target=Controller,args=(depanOut,belakangOut,kiriOut,kananOut))
+        KirimLampu              = Process(target=SensorTraffic,args=(lampuIn,))
+        ControllerUtama         = Process(target=Controller,args=(depanOut,belakangOut,kiriOut,kananOut,lampuOut))
 
+        KirimLampu.start()
         KirimSensorDepan.start()
         KirimSensorBelakang.start()
         KirimSensorKiri.start()
         KirimSensorKanan.start()
         ControllerUtama.start()
 
+        KirimLampu.join()
         KirimSensorDepan.join()
         KirimSensorBelakang.join()
         KirimSensorKiri.join()
