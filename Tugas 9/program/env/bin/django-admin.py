d0f4246aab2c38e3f52e12c94f1bda803cf285e1
aldion amirrul <aldionamirrul@gmail.com>
2019-06-22 11:33:40 +0700

    adding readme, removing double environtmen on tugas 9, collecting requirements.txt on smartcar project

--- Tugas 9/program/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/leii/Documents/My Kuliah/Semester 6/Komputasi paraler/Komputasi-Parallel/Tugas 9/program/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
